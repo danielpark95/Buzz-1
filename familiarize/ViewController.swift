a0c2b41aaa3f28206eac3532ce3e5220d3ab1015
alexswo <alexswo@g.ucla.edu>
2017-05-27 22:50:59 -0700

    Initial Commit

---
+++ familiarize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  familiarize
+//
+//  Created by Alex Oh on 5/27/17.
+//  Copyright © 2017 nosleep. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
