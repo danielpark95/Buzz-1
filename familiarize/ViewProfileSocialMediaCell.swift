63a322481ce067f381f2048e68f5b4655f2faf13
alexswo <alexswo@g.ucla.edu>
2017-08-31 14:48:34 -0700

    Buttons now clickable! Just need to find all the urls for deeplinking

--- familiarize/ViewProfileSocialMediaCell.swift
+++ familiarize/ViewProfileSocialMediaCell.swift
@@ -8,6 +8,12 @@
 import UIKit
 
 class ViewProfileSocialMediaCell: UICollectionViewCell {
+
+    private enum buttonType: Int {
+        case faceBookProfile
+    }
+    
+    let socialMediaAppNameToEnum: [String : Int] = ["faceBookProfile" : buttonType.faceBookProfile.rawValue]
     
     fileprivate let viewProfileSocialMediaCellId = "viewProfileSocialMediaCellId"
     var socialMedia: SocialMedia? {
@@ -15,6 +21,7 @@ class ViewProfileSocialMediaCell: UICollectionViewCell {
             if let imageName = socialMedia?.imageName {
                 let image = UIImage(named: imageName)
                 socialMediaButton.setImage(image, for: .normal)
+                socialMediaButton.tag = socialMediaAppNameToEnum[(socialMedia?.appName)!]!
                 setupViews()
             }
         }
@@ -22,6 +29,7 @@ class ViewProfileSocialMediaCell: UICollectionViewCell {
     
     lazy var socialMediaButton: UIButton = {
         let button = UIManager.makeButton()
+        button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
         return button
     }()
     
@@ -31,5 +39,38 @@ class ViewProfileSocialMediaCell: UICollectionViewCell {
         socialMediaButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
         socialMediaButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
         socialMediaButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    // MARK: - Button Properties
+    func buttonClicked() {
+        var appURL: URL?
+        var safariURL: URL?
+
+        switch socialMediaButton.tag {
+        case buttonType.faceBookProfile.rawValue:
+            // Lmao, in order to get profile id, just scrape the facebook page again.
+            // <meta property="al:ios:url" content="fb://profile/100001667117543">
+            appURL = URL(string: "fb://profile?id=\((socialMedia?.inputName)!)")!
+            safariURL = URL(string: "https://www.facebook.com/\((socialMedia?.inputName)!)")!
+        default: break
+        }
+        
+        // MARK: Open app on phone or load it on safari.
+        if let appURL = appURL, let safariURL = safariURL {
+            if UIApplication.shared.canOpenURL(appURL) {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(appURL)
+                } else {
+                    UIApplication.shared.openURL(appURL)
+                }
+            } else if UIApplication.shared.canOpenURL(safariURL) {
+                //redirect to safari because the user doesn't have facebook application
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(safariURL)
+                } else {
+                    UIApplication.shared.openURL(safariURL)
+                }
+            }
+        }
     }
 }
